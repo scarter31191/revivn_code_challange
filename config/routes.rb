fc0115db6be7315e7c156d49e55197caa466f3f9
scarter31191 <scarter31191@gmaile.com>
2021-10-17 19:17:38 -0400

    gave pickup its own route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :directors do
-        resources :pickups
-      end
+      resources :directors
+      resources :pickups
     end
   end
 
