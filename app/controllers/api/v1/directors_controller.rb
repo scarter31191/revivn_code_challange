1ba153b1c18f5588572457b452d99c300250a4b7
scarter31191 <scarter31191@gmaile.com>
2021-10-17 00:19:07 -0400

    added CORS

--- app/controllers/api/v1/directors_controller.rb
+++ app/controllers/api/v1/directors_controller.rb
@@ -1,51 +1,57 @@
-class Api::V1::DirectorsController < ApplicationController
-  before_action :set_director, only: [:show, :update, :destroy]
-
-  # GET /directors
-  def index
-    @directors = Director.all
-
-    render json: @directors.includes(:pickups), only: [:name]
-  end
-
-  # GET /directors/1
-  def show
-    render json: @director
-  end
-
-  # POST /directors
-  def create
-    @director = Director.new(director_params)
-
-    if @director.save
-      render json: @director, status: :created, location: @director
-    else
-      render json: @director.errors, status: :unprocessable_entity
+module Api
+  module V1
+    class DirectorsController < ApplicationController
+      before_action :set_director, only: [:show, :update, :destroy]
+    
+      # GET /directors
+      def index
+        @directors = Director.all
+    
+        render json: @directors.includes(:pickups), only: [:name]
+      end
+    
+      # GET /directors/1
+      def show
+        render json: @director
+      end
+    
+      # POST /directors
+      def create
+        @director = Director.new(director_params)
+    
+        if @director.save
+          render json: @director, status: :created, location: @director
+        else
+          render json: @director.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # PATCH/PUT /directors/1
+      def update
+        if @director.update(director_params)
+          render json: @director
+        else
+          render json: @director.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # DELETE /directors/1
+      def destroy
+        @director.destroy
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_director
+          @director = Director.find(params[:id])
+        end
+    
+        # Only allow a trusted parameter "white list" through.
+        def director_params
+          params.require(:director).permit(:name)
+        end
     end
   end
-
-  # PATCH/PUT /directors/1
-  def update
-    if @director.update(director_params)
-      render json: @director
-    else
-      render json: @director.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /directors/1
-  def destroy
-    @director.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_director
-      @director = Director.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def director_params
-      params.require(:director).permit(:name)
-    end
 end
+
+
--- app/controllers/api/v1/pickups_controller.rb
+++ app/controllers/api/v1/pickups_controller.rb
@@ -1,51 +1,58 @@
-class Api::V1::PickupsController < ApplicationController
-  before_action :set_pickup, only: [:show, :update, :destroy]
-
-  # GET /pickups
-  def index
-    @pickups = Pickup.all
-
-    render json: @pickups, only: [:director_id, :submission, :location, :product, :status, :employee]
-  end
-
-  # GET /pickups/1
-  def show
-    render json: @pickup
-  end
-
-  # POST /pickups
-  def create
-    @pickup = Pickup.new(pickup_params)
-
-    if @pickup.save
-      render json: @pickup, status: :created, location: @pickup
-    else
-      render json: @pickup.errors, status: :unprocessable_entity
+module Api
+  module V1
+    class PickupsController < ApplicationController
+      before_action :set_pickup, only: [:show, :update, :destroy]
+    
+      # GET /pickups
+      def index
+        @pickups = Pickup.all
+    
+        render json: @pickups, only: [:director_id, :submission, :location, :product, :status, :employee]
+      end
+    
+      # GET /pickups/1
+      def show
+        render json: @pickup
+      end
+    
+      # POST /pickups
+      def create
+        @pickup = Pickup.new(pickup_params)
+    
+        if @pickup.save
+          render json: @pickup, status: :created, location: @pickup
+        else
+          render json: @pickup.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # PATCH/PUT /pickups/1
+      def update
+        if @pickup.update(pickup_params)
+          render json: @pickup
+        else
+          render json: @pickup.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # DELETE /pickups/1
+      def destroy
+        @pickup.destroy
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_pickup
+          @pickup = Pickup.find(params[:id])
+        end
+    
+        # Only allow a trusted parameter "white list" through.
+        def pickup_params
+          params.require(:pickup).permit(:director_id, :submission, :location, :product, :status, :employee)
+        end
     end
   end
-
-  # PATCH/PUT /pickups/1
-  def update
-    if @pickup.update(pickup_params)
-      render json: @pickup
-    else
-      render json: @pickup.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /pickups/1
-  def destroy
-    @pickup.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pickup
-      @pickup = Pickup.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def pickup_params
-      params.require(:pickup).permit(:director_id, :submission, :location, :product, :status, :employee)
-    end
 end
+
+
+
